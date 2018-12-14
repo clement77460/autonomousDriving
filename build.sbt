61c4b7f4125a62f7d8cacb4b7356ef7a025d718d
clement <clement.jaworski@hotmail.com>
2018-12-14 14:54:27 +0100

    structure du projet

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "autonomousDriving"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.0"
+//libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.4.0"
+//libraryDependencies += "org.apache.bahir" %% "spark-sql-cloudant" % "2.4.0"
---
+++ src/main/scala/Programm.scala
@@ -0,0 +1,52 @@
+import org.apache.spark.streaming._
+import org.apache.spark.{SparkConf, SparkContext, sql}
+
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+object Programm {
+
+  def main(ars:Array[String])={
+    println("main")
+    sparkStreaming()
+
+  }
+
+  def sparkStreaming()={
+    println("start sparkStreaming")
+    val conf = new SparkConf().setMaster("local[*]")
+      .setAppName("Simple Streaming Application")
+    val ssc = new StreamingContext(conf, Seconds(4))
+
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+
+    stream.map(record => (record.key, record.value)).foreachRDD(_.foreach{x =>
+      println(x)
+      println("############################################################################################################")
+      println("############################################################################################################")
+      println("############################################################################################################")})
+
+    ssc.start()             // Start the computation
+    ssc.awaitTermination()  // Wait for the computation to terminate
+    println("end sparkStreaming")
+  }
+}
